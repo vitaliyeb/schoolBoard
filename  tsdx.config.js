b2eac780169b5aefc5a74e93d72da415e9cae08b
vitaliyeb <vlvlv.vovov@bk.ru>
2021-04-28 18:34:48 +0500

    пытаюсь подключить стили

---  tsdx.config.js
+++  tsdx.config.js
@@ -1,1 +1,21 @@
-
+const postcss = require('rollup-plugin-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+module.exports = {
+    rollup(config, options) {
+        console.log('asdsa')
+        config.plugins.push(
+            postcss({
+                plugins: [
+                    autoprefixer(),
+                    cssnano({
+                        preset: 'default',
+                    }),
+                ],
+                inject: true,
+                extract: true,
+            })
+        );
+        return config;
+    },
+};
