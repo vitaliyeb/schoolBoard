ec4394667fa20358e83c7a0bf6eb57492f2a126d
vitaliyeb <vlvlv.vovov@bk.ru>
2021-05-17 20:51:10 +0500

    перепсываю стору

--- src/store.ts
+++ src/store.ts
@@ -3,7 +3,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import boardReduser from './redusers/board';
 
 export default const store = configureStore({
-    boardReduser,
+    reducer: boardReduser,
 })
 
 
