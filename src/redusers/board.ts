bccb9aafb227265ca21cc1137c237a261db02f70
vitaliyeb <vlvlv.vovov@bk.ru>
2021-05-25 22:09:08 +0500

    сделал нгачальный стейт

--- src/redusers/board.ts
+++ src/redusers/board.ts
@@ -1,6 +1,9 @@
 import { createReducer } from '@reduxjs/toolkit'
 
+const initialState = {
+    counter: 0
+}
 
-export default createReducer(initialState = {counter: 0}, () => {
+export default createReducer(initialState, () => {
 
 });
